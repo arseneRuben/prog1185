9dfeb64be211e347b944faca5383ae9eb6c3126b
arsene <fopoar@gmail.com>
2024-05-11 21:37:13 -0300

    devoir 3 et solution devoirs 2

---
+++ solutions/04-fonction/exercice/10-generateur/js/generateur.js
@@ -0,0 +1,12 @@
+
+function createItem(a)
+{
+  return "<li>"+a+"</li>";
+}
+
+ function createList(tableau)
+ {
+  
+  return "<ul>" +createItem(tableau[0]) +createItem(tableau[1]) +createItem(tableau[2]) +createItem(tableau[3])+"</ul>";
+
+ }
