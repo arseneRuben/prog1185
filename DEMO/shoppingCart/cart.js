1132e2a9e7e4611cbf1d6f57e0616897121c3f46
Alseny Kaba <alsenykaba98@gmail.com>
2024-05-23 10:08:26 -0300

    Solution Alseny_kaba

--- DEMO/shoppingCart/cart.js
+++ DEMO/shoppingCart/cart.js
@@ -1,74 +1,74 @@
 let books = [
-{
-	isbn : "1",
-	image : "angular.jpg",
-	stock : 34,
-	title : " Angular JS",
-	price : 56
-},
-{
-	isbn : "2",
-	image : "ja_angular.jpg",
-	stock : 4,
-	title : " JavaScript et Angular",
-	price : 36
-},
-{
-	isbn : "3",
-	image : "jquery.jpg",
-	stock : 34,
-	title : " JQuery",
-	price : 56
-},
-{
-	isbn : "4",
-	image : "js.jpg",
-	stock : 14,
-	title : " JavaScript : Le guide complet",
-	price : 6
-},
-{
-	isbn : "5",
-	image : "js_jquery.jpg",
-	stock : 4,
-	title : " JavaScript et JQuery",
-	price : 46
-},
-{
-	isbn : "6",
-	image : "node_js.jpg",
-	stock : 4,
-	title : " Node Js",
-	price : 38
-},
-{
-	isbn : "7",
-	image : "php.jpg",
-	stock : 4,
-	title : " Php",
-	price : 46
-},
-{
-	isbn : "8",
-	image : "php_mysql.jpg",
-	stock : 4,
-	title : " Php et Mysql",
-	price : 38
-},
-{
-	isbn : "9",
-	image : "react.jpg",
-	stock : 12,
-	title : " React",
-	price : 98
-},
-{
-	isbn : "10",
-	image : "symfony_php.jpg",
-	stock : 12,
-	title : " Php et Symfony",
-	price : 46
-},
+	{
+		isbn: "1",
+		image: "angular.jpg",
+		stock: 34,
+		title: " Angular JS",
+		price: 56
+	},
+	{
+		isbn: "2",
+		image: "ja_angular.jpg",
+		stock: 4,
+		title: " JavaScript et Angular",
+		price: 36
+	},
+	{
+		isbn: "3",
+		image: "jquery.jpg",
+		stock: 34,
+		title: " JQuery",
+		price: 56
+	},
+	{
+		isbn: "4",
+		image: "js.jpg",
+		stock: 14,
+		title: " JavaScript : Le guide complet",
+		price: 6
+	},
+	{
+		isbn: "5",
+		image: "js_jquery.jpg",
+		stock: 4,
+		title: " JavaScript et JQuery",
+		price: 46
+	},
+	{
+		isbn: "6",
+		image: "node_js.jpg",
+		stock: 4,
+		title: " Node Js",
+		price: 38
+	},
+	{
+		isbn: "7",
+		image: "php.jpg",
+		stock: 4,
+		title: " Php",
+		price: 46
+	},
+	{
+		isbn: "8",
+		image: "php_mysql.jpg",
+		stock: 4,
+		title: " Php et Mysql",
+		price: 38
+	},
+	{
+		isbn: "9",
+		image: "react.jpg",
+		stock: 12,
+		title: " React",
+		price: 98
+	},
+	{
+		isbn: "10",
+		image: "symfony_php.jpg",
+		stock: 12,
+		title: " Php et Symfony",
+		price: 46
+	},
 
 
 ]
@@ -87,16 +87,38 @@ let books = [
 
 
 const tBody = document.getElementsByTagName('tbody')[0];
-books.map( book =>  {
-			let tr = document.createElement('tr');
-			let firstTd = document.createElement('td');
-			let img = document.createElement('img');
-			img.src = `img/${book.image}`;
-			img.alt = book.image;
-			firstTd.append(img);
-			tr.append(firstTd);
-			tBody.prepend(tr);
-		}
-	)
+books.map(book => {
+	let tr = document.createElement('tr');
+	let firstTd = document.createElement('td');
+	let img = document.createElement('img');
+	img.src = `img/${book.image}`;
+	img.alt = book.image;
+	firstTd.append(img);
+	tr.append(firstTd);
+	tBody.prepend(tr);
+
+	let qtyTd = document.createElement('td');
+	let inputElement = document.createElement('input');
+	inputElement.min=0;
+	inputElement.value=1;
+	inputElement.max = book.stock;
+	inputElement.type= 'number';
+	qtyTd.append(inputElement);
+	tr.append(qtyTd);
+
+	let titleTd = document.createElement('td');
+	titleTd.textContent = book.title;
+	tr.append(titleTd);
+
+	let priceTd = document.createElement('td');
+	priceTd.textContent = `$${book.price}`;
+	tr.append(priceTd);
+
+	let totalTd = document.createElement('td');
+	totalTd.textContent = `$${book.price}`;
+	tr.appendChild(totalTd);
+}
+)
+
 
 
