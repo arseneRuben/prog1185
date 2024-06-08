af07b41c41b19fd78dd5b6d833e136318dea9e27
arsene <fopoar@gmail.com>
2024-06-08 17:41:15 -0300

    Enonce de rattrapage

---
+++ Rattrapage/70-cascade-select/question.js
@@ -0,0 +1,10 @@
+/**
+ * Exercice - cascade-select
+ *
+ * Consignes:
+ * Créer le fichier js/cascade-select.js à partir du code suivant.
+ * Compléter le code manquant pour obtenir le résultat des maquettes.
+ *
+ * Note: L'objet COUNTRIES n'est pas un Array, allez voir la documentation suivante pour pouvoir l'utiliser: Object.keys()
+ */
+'use strict'
